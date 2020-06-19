f9c069ea36f604fe116355e8a67bd1b8ac30d4d9
Dziuda.Alicja <215713@edu.p.lodz.pl>
2020-06-19 12:34:15 +0200

    app connected with postgresql, first outline of app, ability to login using proper data saved in database

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user and user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to admin_url, notice: "Zostałeś zalogowany"
+    else
+      redirect_to login_url, alert: "Hasło lub emeil są nieprawidłowe"
+    end
+  end
+
+  def destroy
+	session[:user_id] = nil
+    redirect_to login_url
+  end
+end
