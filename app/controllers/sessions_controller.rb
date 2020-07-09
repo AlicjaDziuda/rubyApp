1ea2138473e9c769b88c8205e8143b45dfdcac90
Krystian Bendinger <krystian.bendinger@gmail.com>
2020-07-10 01:44:34 +0200

    added home page with login based on bootstrap

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
-    if user and user.authenticate(params[:password])
+    user = User.find_by(email: params[:session][:email])
+    if user and user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       redirect_to booked_lecture_rooms_url, notice: "Zostałeś zalogowany"
     else
@@ -12,6 +12,8 @@ class SessionsController < ApplicationController
     end
   end
 
+  
+  
   def destroy
 	session[:user_id] = nil
     redirect_to login_url
