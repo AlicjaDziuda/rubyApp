409aa9eb05267fd6aecce40b883ee2277d39f5bb
Dziuda.Alicja <215713@edu.p.lodz.pl>
2020-07-10 18:42:39 +0200

    logging repared

--- app/models/booked_lecture_room.rb
+++ app/models/booked_lecture_room.rb
@@ -5,7 +5,7 @@ class BookedLectureRoom < ApplicationRecord
 	validates :name, :uniqueness => { :scope => :data, :scope => :time,
     :message => "Tego sala jest juz wtedy zarezerwowana" }
     validates :hours, :numericality => { :greater_than => 0 }
-    validates :name, :uniqueness => { :scope => :data, :scope => (:hours + 2),
+    validates :name, :uniqueness => { :scope => :data, :scope => (:hours),
     :message => "Tego sala jest juz wtedy zarezerwowana" }
 end
 
